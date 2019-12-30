330b058cc5a66468acf33cbfb6629651a23de82f
hacksaw390 <shamimdewan343@gmal.com>
2019-12-30 10:31:02 +0600

    parallax

---
+++ js/custom.js
@@ -0,0 +1,91 @@
+$(document).ready(function(){
+$('.fimg').parallaxie({
+    speed: 0.5
+  });
+$('.simg').parallaxie({
+    speed: .5
+  });
+$('.timg').parallaxie({
+    speed: .5
+  });
+$('.fimg').parallaxie({
+    speed: 0.5
+  });
+
+
+
+});
+
+ // 'colvis'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// window.onload = function(){
+//     $(".load").css("display", "none");
+// }
+
+
+// $(function(){
+
+
+//   var name = $("#name").attr("type");
+  
+//   $("#check").click(function(){
+//       var btnfirstvalu = $(this).text();
+
+//       if($(this).attr("check") == "none"){
+//         $("#check").text("Click Kora Hoyeche");
+//         $(this).attr("check", "checking");
+//         $(this).addClass("classtype");
+//       }else{
+//         $("#check").text("Click");
+//         $(this).attr("check", "none");
+//         $(this).removeClass("classtype");
+//       }
+
+//   });
+
+
+//   // $("#check").slice();
+
+
+
+
+
+// });
+
+// // console.log("This is my test jquery");
